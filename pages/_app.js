202c6c114eb77acc3e8f136a4562bfbae2c51f2e
numaan qureshi <nomannaeem985@gmail.com>
2024-11-17 13:35:24 +0530

    Integrated Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,35 @@ import NavBar from "@/components/NavBar/NavBar";
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 import Head from "next/head";
+import { useEffect } from "react";
 
 const poppins = Poppins({ weight: ['500', '700'], subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Add the Google Tag script
+    const script = document.createElement("script");
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-V5XCECZ6JB";
+    script.async = true;
+    document.head.appendChild(script);
+
+    // Initialize Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-V5XCECZ6JB");
+  }, []);
+
   return (
     <>
       <Head>
         <link rel="shortcut icon" href="/hawkflitnewlogo.png" type="image/png" />
-        <meta name="google-site-verification" content="f9c5_PzRfCj9bG3corfANyFsbYaW3I90yMxyKfWpDLM" />
+        <meta
+          name="google-site-verification"
+          content="f9c5_PzRfCj9bG3corfANyFsbYaW3I90yMxyKfWpDLM"
+        />
       </Head>
       <div className={poppins.className}>
         <NavBar />
